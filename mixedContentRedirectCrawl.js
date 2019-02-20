c68a22a2d77afa5fb625e2edbdfc9669b88a012b
Matt James <mattjamesvisuals@gmail.com>
2019-02-20 14:08:09 -0700

    moved scanMixedContent funcion to its own file

--- mixedContentRedirectCrawl.js
+++ mixedContentRedirectCrawl.js
@@ -1,87 +1,11 @@
 const fetch = require('node-fetch');
-const puppeteer = require('puppeteer');
-const lodash = require('lodash');
 const parseCSV = require('./parseCSV').parseCSV;
 const Table = require('cli-table');
+const lodash = require('lodash');
 const chalk = require('chalk');
+const scanMixedContent = require('./scanMixedContentFunction').scanMixedContent;
 
 
-// This function does the scanning for mixed content errors using puppeteer
-const scanMixedContent = async (sites) => {
-  const browser = await puppeteer.launch({
-    headless: true
-  })
-  const page = await browser.newPage()
-
-  // Get a handle for the client used by the page object to communicate with
-  // the browser through the DevTools protocol
-  const devToolsClient = page._client
-
-  let failedRequests = new Map()
-  let mixedContentIssues = new Set()
-  let currentRunSite = '';
-
-  // Event fired when a request fired by the page failed
-  page.on('requestfailed', request => {
-    //console.log(request);
-    // Store a reference to that request, we'll need to get more information
-    // about Mixed Content errors later
-    const siteIndex = lodash.findIndex(sites, function(o) {
-      return o.siteName == currentRunSite
-    });
-    sites[siteIndex].mixedContent.push({
-      url: request.url(),
-      resourceType: request.resourceType(),
-      method: request.method()
-    });
-    // failedRequests.set(currentRunSite, {
-    //   url: request.url(),
-    //   resourceType: request.resourceType(),
-    //   method: request.method()
-    // });
-  });
-  page.on('close', request => {
-    return failedRequests;
-  });
-
-  // If a request failed due to a Mixed Content issue, log it
-  page._client.on('Network.loadingFailed', event => {
-    if (Object.is(event.blockedReason, 'mixed-content')) {
-      mixedContentIssues.add(event.requestId)
-    }
-  });
-
-  for (let i = 0; i < sites.length; i++) {
-    //console.log(sites[i]);
-    const {
-      url,
-      status,
-      siteName
-    } = sites[i];
-    currentRunSite = siteName;
-    if (status < 400) {
-      //console.log(`attempting to scan mixed content errors for ${siteName}`)
-      const promise = page.waitForNavigation({
-        waitUntil: 'networkidle2'
-      });
-      await page.goto(`${url}`);
-      await promise;
-    }
-
-  }
-
-  // for (let requestId of mixedContentIssues) {
-  //   console.log(failedRequests);
-  //   const {
-  //     method,
-  //     url,
-  //     resourceType
-  //   } = failedRequests.get(requestId);
-  //   //console.log(`Mixed Content warning when sending ${method} request to ${url} (${resourceType})`)
-  // }
-  browser.close();
-  return sites;
-}
 
 // This is where the magic happens
 
@@ -100,7 +24,7 @@ parseCSV("./containerTagUrls.csv").then((data) => {
     try {
       const response = await fetch(url, {
         redirect: 'follow',
-        follow: 3
+        follow: 1
       });
       return {
         response: response,
@@ -121,7 +45,6 @@ parseCSV("./containerTagUrls.csv").then((data) => {
     }
 
   })).then((responses) => {
-    //console.log(responses);
     return {
       responses: responses
     };
@@ -156,7 +79,6 @@ parseCSV("./containerTagUrls.csv").then((data) => {
   results.unshift(google);
   return scanMixedContent(results);
 }).then((mixedContentErrors) => {
-  //console.log(mixedContentErrors);
   const sorted = lodash.sortBy(mixedContentErrors, [function(o) {
     return String(o.status);
   }]);
@@ -173,8 +95,6 @@ parseCSV("./containerTagUrls.csv").then((data) => {
     let status = obj.status;
     let siteName = obj.siteName;
 
-
-
     if (status == 'too many re-directs') {
       status = chalk.yellow(status);
       siteName = chalk.yellow(siteName);
@@ -185,7 +105,6 @@ parseCSV("./containerTagUrls.csv").then((data) => {
       siteName = chalk.red(siteName);
     } else status = chalk.green(status);
     siteName = chalk.green(siteName);
-
 
     return [siteName, obj.url, status, JSON.stringify(obj.mixedContent)];
   });
---
+++ scanMixedContentFunction.js
@@ -0,0 +1,69 @@
+const puppeteer = require('puppeteer');
+const lodash = require('lodash');
+
+// This function does the scanning for mixed content errors using puppeteer
+const scanMixedContent = async (sites) => {
+  const browser = await puppeteer.launch({
+    headless: true
+  })
+  const page = await browser.newPage()
+
+  // Get a handle for the client used by the page object to communicate with
+  // the browser through the DevTools protocol
+  const devToolsClient = page._client
+
+  let failedRequests = new Map()
+  let mixedContentIssues = new Set()
+  let currentRunSite = '';
+
+  // Event fired when a request fired by the page failed
+  page.on('requestfailed', request => {
+    // Store a reference to that request, we'll need to get more information
+    // about Mixed Content errors later
+    const siteIndex = lodash.findIndex(sites, function(o) {
+      return o.siteName == currentRunSite
+    });
+    sites[siteIndex].mixedContent.push({
+      url: request.url(),
+      resourceType: request.resourceType(),
+      method: request.method()
+    });
+
+  });
+  page.on('close', request => {
+    return failedRequests;
+  });
+
+  // If a request failed due to a Mixed Content issue, log it
+  page._client.on('Network.loadingFailed', event => {
+    if (Object.is(event.blockedReason, 'mixed-content')) {
+      mixedContentIssues.add(event.requestId)
+    }
+  });
+
+  for (let i = 0; i < 5; i++) {
+    const {
+      url,
+      status,
+      siteName
+    } = sites[i];
+    currentRunSite = siteName;
+    if (status < 400) {
+      console.log(`attempting to scan mixed content errors for ${siteName}`)
+      const promise = page.waitForNavigation({
+        waitUntil: 'networkidle2'
+      });
+      await page.goto(`${url}`);
+      await promise;
+    }
+
+  }
+
+
+  browser.close();
+  return sites;
+}
+
+module.exports = {
+  scanMixedContent
+};
